e0b704a55051ee149eda4324b0a6a342cc940241
ololade <ololadelawal@gmail.com>
2022-10-18 13:40:14 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -32,8 +32,10 @@ int _printf(const char *format, ...)
 				chars_printed += print_binary((unsigned int)va_arg(args, int));
 			else if (format[i] == 'r')
 				chars_printed += print_reverse(va_arg(args, char *));
-			else if (format[i] == 'R')
-				chars_printed += print_rot13(va_arg(args, char *));
+			/**
+			*else if (format[i] == 'R')
+				*chars_printed += print_rot13(va_arg(args, char *));
+				*/
 			else if (format[i] == 'o' || format[i] == 'u' ||
 			format[i] == 'x' || format[i] == 'X')
 				chars_printed += print_odh(format[i], (unsigned int)va_arg(args, int));

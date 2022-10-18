13bdb6f97e1f7d271221e433c3bbf2518a56a94a
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2022-10-18 23:03:36 +0100

    update printf.c

--- _printf.c
+++ _printf.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _printf - produces output according to a format.
+ * _printf - produces output according to a format
  * @format: character string
  *
  * Return: number of characters printed (excluding the null byte used to end
